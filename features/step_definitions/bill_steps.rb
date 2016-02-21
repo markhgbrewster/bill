32fc980868d21c7c9bfc926c2a605efcf9ae739d
Mark Brewster <m.brewster@volcanic.co.uk>
2016-02-21 21:47:15 +0000

    tests updated

--- features/step_definitions/bill_steps.rb
+++ features/step_definitions/bill_steps.rb
@@ -6,6 +6,6 @@ Given(/^I am on the view my bill page$/) do
 end
 
 Then(/^I see my bill$/) do
-  expect(page).to have_css('div.container') 
+  expect(page).to have_css('div.skycom-container') 
   expect(page).to have_content('Your Statement')
 end
